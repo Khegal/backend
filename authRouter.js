292b88d5f3fdfdc83034118fc774ac8c422edc0e
24HP7431 24HP7431 <24HP7431@admins-iMac.local>
2024-11-25 18:38:58 +0800

    main

--- authRouter.js
+++ authRouter.js
@@ -1,122 +1,65 @@
 import express from "express";
-import * as fs from "fs";
-import { nanoid } from "nanoid";
-import { object, string } from "yup";
 import bcrypt from "bcrypt";
-const saltRounds = 10;
+import UserModel from "./models/user-model.js";
 
 const router = express.Router();
-
-let users = [];
-
-let signUpSchema = object({
-  userName: string().required("Must not be empty"),
-  password: string().required("Must not be empty").min(8),
-  email: string().email("Must be a valid email").required("Must not be empty"),
-});
-
-let loginSchema = object({
-  email: string().email("Must be a valid email").required("Must not be empty"),
-  password: string().required("Must not be empty").min(8),
-});
-
-let changePassword = object({
-  email: string().email("Must be a valid email").required("Must not be empty"),
-  password: string().required("Must not be empty").min(8),
-  newPassword: string(),
-});
-
-let changeEmail = object({
-  email: string().email("Must be a valid email").required("Must not be empty"),
-  password: string().required("Must not be empty").min(8),
-  newEmail: string(),
-});
-
-if (fs.existsSync("users.json")) {
-  const data = fs.readFileSync("users.json", "utf-8");
-  if (data.length > 0) {
-    users = JSON.parse(data);
-  }
-}
-
-router.get("", (req, res) => {
-  if (users.length === 0) {
-    return res.status(404).json({ error: "No users found" });
-  }
-  res.json(users);
-});
+const saltRounds = 10;
 
 router.post("/signup", async (req, res) => {
   try {
-    const { userName, email, password } = await signUpSchema.validate(req.body);
-
-    users = JSON.parse(fs.readFileSync("users.json", "utf-8"));
-
-    const emailCheck = users.find((user) => user.email === email);
-    if (emailCheck) {
-      return res.status(400).send({ message: "Email is already registered" });
+    const { userName, email, password } = req.body;
+    const existingUser = await UserModel.findOne({ email });
+    if (existingUser) {
+      return res.status(409).send({ message: "User already exists" });
     }
-
     const hashedPassword = await bcrypt.hash(password, saltRounds);
-
-    const newUser = {
-      id: nanoid(),
-      userName,
+    const newUser = await UserModel.create({
       email,
       password: hashedPassword,
-    };
-
-    users.push(newUser);
-    fs.writeFileSync("users.json", JSON.stringify(users));
-
-    res.status(201).json({ message: "New user created" });
+      userName,
+    });
+    return res
+      .status(201)
+      .send({ message: "User created successfully", user: newUser });
   } catch (error) {
-    res.status(400).json({ error: error.errors });
+    return res
+      .status(500)
+      .send({ message: "An error occurred", error: error.message });
   }
 });
 
 router.post("/login", async (req, res) => {
   try {
-    const { email, password } = await loginSchema.validate(req.body);
-
-    const user = users.find((user) => user.email === email);
-
+    const { email, password } = req.body;
+    const user = await UserModel.findOne({ email });
     if (!user) {
       return res.status(404).send({ message: "User not found" });
     }
-
     const passwordMatch = await bcrypt.compare(password, user.password);
-
     if (!passwordMatch) {
       return res.status(400).send({ message: "Incorrect password" });
     }
-
     return res.status(200).send({ message: "Successful login" });
   } catch (error) {
-    res.status(400).json({ error: error.errors });
+    return res
+      .status(500)
+      .send({ message: "An error occurred", error: error.message });
   }
 });
 
 router.put("/changePassword", async (req, res) => {
   try {
-    const { email, password, newPassword } = await changePassword.validate(
-      req.body
-    );
-    const user = users.find((user) => user.email === email);
-
+    const { email, password, newPassword } = req.body;
+    const user = await UserModel.findOne({ email });
     if (!user) {
       return res.status(404).send({ message: "User not found" });
     }
-
     const passwordMatch = await bcrypt.compare(password, user.password);
     if (!passwordMatch) {
       return res.status(400).send({ message: "Incorrect password" });
     }
-
-    // Update password and save to file
     user.password = await bcrypt.hash(newPassword, saltRounds);
-    fs.writeFileSync("users.json", JSON.stringify(users));
-
+    await user.save();
     return res.status(200).send({ message: "Password updated successfully" });
   } catch (error) {
     return res
@@ -127,35 +70,21 @@ router.put("/changePassword", async (req, res) => {
 
 router.put("/changeEmail", async (req, res) => {
   try {
-    const { email, password, newEmail } = await changeEmail.validate(req.body);
-    const user = users.find((user) => user.email === email);
-
+    const { email, password, newEmail } = req.body;
+    const user = await UserModel.findOne({ email });
     if (!user) {
       return res.status(404).send({ message: "User not found" });
     }
-
-    const newEmailTaken = users.some(
-      (userWithSame) => userWithSame.email === newEmail
-    );
-
-    if (newEmailTaken) {
-      return res.status(400).send({ message: "Email already in use" });
-    }
-
     const passwordMatch = await bcrypt.compare(password, user.password);
     if (!passwordMatch) {
       return res.status(400).send({ message: "Incorrect password" });
     }
-
-    if (user.email === newEmail) {
-      return res
-        .status(400)
-        .send({ message: "New email is the same as the current email" });
+    const newEmailTaken = await UserModel.findOne({ email: newEmail });
+    if (newEmailTaken) {
+      return res.status(400).send({ message: "Email already in use" });
     }
-
     user.email = newEmail;
-    fs.writeFileSync("users.json", JSON.stringify(users));
-
+    await user.save();
     return res.status(200).send({ message: "Email updated successfully" });
   } catch (error) {
     return res
