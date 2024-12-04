ac39cc154ed90617307182cfc8d6528fed342556
Khegal <khegal95@gmail.com>
2024-12-04 18:51:47 +0800

    main

--- index.js
+++ index.js
@@ -2,12 +2,14 @@ import express from "express";
 import authRouter from "./authRouter.js";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import cors from "cors";
 
 dotenv.config({ path: ".env" });
 
 const PORT = process.env.PORT || 3333;
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use("/users", authRouter);
 
