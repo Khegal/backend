763098e7ead885b2f703bc3043b43eaf8719dabe
Khegal <khegal95@gmail.com>
2025-01-08 18:53:54 +0800

    main

--- models/post-model.js
+++ models/post-model.js
@@ -1,4 +1,4 @@
-import mongoose, { Mongoose } from "mongoose";
+import mongoose from "mongoose";
 
 const postSchema = new mongoose.Schema(
   {
@@ -6,10 +6,26 @@ const postSchema = new mongoose.Schema(
     mediaUrl: { type: String },
     likeCount: { type: Number, default: 0 },
     commentCount: { type: Number, default: 0 },
-    user: { type: mongoose.Schema.Types.ObjectId, ref: "users", required: true },
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "users",
+      required: true,
+    },
   },
-  { timestamps: true }
+  { toJSON: { virtuals: true }, toObject: { virtuals: true }, timestamps: true }
 );
+
+postSchema.virtual("comments", {
+  ref: "comments",
+  localField: "_id",
+  foreignField: "post",
+});
+
+postSchema.virtual("likes", {
+  ref: "likes",
+  localField: "_id",
+  foreignField: "post",
+});
 
 const PostModel = mongoose.model("posts", postSchema);
 
--- routers/post-router.js
+++ routers/post-router.js
@@ -1,12 +1,21 @@
 import express from "express";
 import PostModel from "../models/post-model.js";
 import { authMiddleware } from "../middlewares/auth-middleware.js";
+import CommentModel from "../models/comment-model.js";
+import LikeModel from "../models/like-model.js";
 
 const router = express.Router();
 
 router.get("/", async (req, res) => {
   const posts = await PostModel.find({})
-    .populate("user", "_id email username fullname")
+    .populate("user", "profileUrl username")
+    .populate({
+      path: "comments",
+      populate: {
+        path: "user",
+        select: "profileUrl username",
+      },
+    })
     .sort({ createdAt: -1 });
   return res.send(posts);
 });
@@ -23,9 +32,67 @@ router.post("/", authMiddleware, async (req, res) => {
 });
 
 router.get("/user/:_id", async (req, res) => {
-  const { _id } = req.params;
-  const posts = await PostModel.find({ user: _id });
-  return res.send(posts);
+  try {
+    const { _id } = req.params;
+    const posts = await PostModel.find({ user: _id });
+    return res.send(posts);
+  } catch {
+    return res
+      .status(500)
+      .json({ message: "Server can't handle that request" });
+  }
+});
+
+router.post("/:postId/comments", authMiddleware, async (req, res) => {
+  try {
+    const { postId } = req.params;
+    const { comment } = req.body;
+    const user = req.user;
+    const newComment = await CommentModel.create({
+      comment,
+      post: postId,
+      user: user._id,
+    });
+    return res.send(newComment);
+  } catch (err) {
+    return res
+      .status(500)
+      .json({ message: "Server can't handle that request" });
+  }
+});
+
+router.post("/:postId/like", authMiddleware, async (req, res) => {
+  try {
+    const { postId } = req.params;
+    const user = req.user;
+
+    const existingLike = await LikeModel.findOne({
+      post: postId,
+      user: user._id,
+    });
+
+    if (existingLike) {
+      await LikeModel.findByIdAndDelete(existingLike._id);
+
+      await PostModel.findByIdAndUpdate(postId, { $inc: { likeCount: -1 } });
+
+      return res.status(200).json({ message: "Post unliked" });
+    } else {
+      const newLike = await LikeModel.create({
+        post: postId,
+        user: user._id,
+      });
+
+      await PostModel.findByIdAndUpdate(postId, { $inc: { likeCount: 1 } });
+
+      return res.status(200).send(newLike);
+    }
+  } catch (err) {
+    console.error("Error toggling like:", err);
+    return res
+      .status(500)
+      .json({ message: "Server can't handle that request" });
+  }
 });
 
 export default router;
